7eee448b41eedef9423e56513dd10852188e834b
Rahul <royrahulrk12@gmail.com>
2023-04-04 23:42:36 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react'
+import Header from "./Components/Header";
+import HomePage from "./Components/HomePage";
+import DetailPage from "./Components/DetailPage";
+import Error from "./Components/Error";
+import axios from 'axios'
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
+const[data,setData]=useState([])
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching first API:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage data={data} />} />
+          <Route path="/HomePage" element={<HomePage data={data}/>} />
+          <Route path="/DetailPage" element={<DetailPage data={data}/>} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
   );
 }
+
 
 export default App;
